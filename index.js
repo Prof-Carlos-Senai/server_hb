9c56a5bf51ba0c5a84dbfefcd7c5cd9525b7a6fc
Carlos <silva@edu.sc.senai.br>
2023-05-10 05:43:44 -0300

    consulta BD e views

--- index.js
+++ index.js
@@ -14,8 +14,29 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 /* ----------------------------------------------- */
+app.get('/consultar', async (req,res)=>{
+    const reg = await Usuario.findOne({raw: true, where: {id: 1}})
+    // console.log(reg)
+    // console.log(reg.id)
+    // console.log(reg.nome)
+    // console.log(reg.idade)
+
+    res.render('consulta', {valor: reg})
+})
+
+app.get('/listar', async (req,res)=>{
+    const dados = await Usuario.findAll({raw: true})
+    // console.log(dados[0].id)
+    // console.log(dados[0].nome)
+    // console.log(dados[0].idade)
 
 
+    res.render('lista', {valor: dados})
+})
+
+app.get('/', (req,res)=>{
+    res.render('home')
+})
 
 /* ----------------------------------------------- */
 conn.sync().then(()=>{
